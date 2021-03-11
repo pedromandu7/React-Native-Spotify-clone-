7d9c87e690d8d556ff033eb5b2826f18ab873585
Pedro Mandú <pedromandu7@gmail.com>
2021-03-11 16:31:18 -0300

    Initializing HomeScreen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import HomeScreen from './screens/home';
+import {createStackNavigator, StackView} from '@react-navigation/stack';
+import PlayScreen from './screens/play';
 import DetailsScreen from './screens/details';
 import SearchScreen from './screens/search';
 
@@ -11,20 +11,21 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Play">
+        <Stack.Screen name="Home" component="HomeScreen" options={{headerShown: true}}/>
         <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Details"
-          component={DetailsScreen}
+          name="Play"
+          component={PlayScreen}
           options={{headerShown: false}}
         />
         <Stack.Screen
           name="Search"
           component={SearchScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
           options={{headerShown: false}}
         />
       </Stack.Navigator>
